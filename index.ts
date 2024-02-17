7af7185022b951d6bda351f940d832fee97978a7
Naborisk <github@naborisk.com>
2024-02-18 06:21:03 +0900

    work with latest concurrent

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, CoreMessage } from '@concurrent-world/client'
+import { Client } from '@concurrent-world/client'
 import * as cowsay from 'cowsay'
 import { rollDice } from './modules/dice'
 
@@ -42,135 +42,106 @@ client.newSocket().then(async socket => {
 
   await socket.waitOpen()
   socket.listen([postStream], async s => {
-    console.log('listening')
-    console.log(s.type)
+    if (s.body.author === ccid) return
+    console.log(s)
 
-    client.getMessage(s.body.id, s.body.author).then(m => {
-      m?.reply([postStream], 'test')
-    })
+    // the message object
+    const m = await client.getMessage(s.body.id, s.body.author)
 
-    //   client.api.createAssociation(
-    //     'https://raw.githubusercontent.com/totegamma/concurrent-schemas/master/associations/reply/0.0.1.json',
-    //     { body: 'aaaaaa' },
-    //     s.body.id,
-    //     s.body.author,
-    //     'messages',
-    //     [postStream]
-    //   )
+    const message = s.body.payload.body
+
+    const command = s.body.payload.body.body
+      .slice(commandPrefix.length)
+      .trimStart()
+      .split(' ')[0]
+
+    console.log(`command: ${command}`)
+
+    const args = message.body
+      .slice(commandPrefix.length + command.length + 1)
+      .trimStart()
+      .split(' ')
+
+    if (message.body && message.body.startsWith(commandPrefix)) {
+      console.log(`command: ${command}`)
+      console.log(`args: ${typeof args} ${JSON.stringify(args)}`)
+      console.log(`s.body.id, s.body.author: ${s.body.id}, ${s.body.author}`)
+
+      switch (command) {
+        case `ping`:
+          await m?.reply([postStream], `pong`)
+          break
+
+        case 'echo':
+          await m?.reply([postStream], args.join(' ').trimStart())
+          break
+
+        case 'cowsay':
+          await m?.reply(
+            [postStream],
+            '```' + cowsay.say({ text: args.join(' ') }) + '```'
+          )
+          break
+
+        case 'cowthink':
+          await m?.reply(
+            [postStream],
+            '```' + cowsay.think({ text: args.join(' ') }) + '```'
+          )
+          break
+
+        case 'roll':
+          const [count, sides] = args.join('').split('d').map(Number)
+          const arr: Number[] = Array.from(Array(Number(count)))
+          const res = arr.map(() => rollDice(sides.toString()))
+
+          await m?.reply(
+            [postStream],
+            `${count}個の${sides}面サイコロを振りました
+              合計: ${res.reduce((acc, cur) => acc + cur)}\n${res.join(' ')}`
+          )
+          break
+
+        case 'jpdict':
+          const proc = Bun.spawn(['myougiden', ...args])
+          const text = await new Response(proc.stdout).text()
+          await m?.reply([postStream], text)
+          break
+
+        case 'current':
+          await m?.reply(
+            [postStream],
+            `只今${new Date().toLocaleTimeString('ja-JP')}でございます。`
+          )
+          break
+
+        case 'help':
+          await m?.reply(
+            [postStream],
+            '```' +
+              `
+    コマンド一覧:
+      ping: pongを返す
+        /con ping
+      cowsay: Linuxのcowsayコマンドと同じ
+        /con cowsay <text>
+      cowthink: Linuxのcowthinkコマンドと同じ
+        /con cowthink <text>
+      roll: サイコロを振る
+        /con roll <個数>d<面数>
+      jpdict: 辞書で単語を検索
+        /con jpdict <word>`
+          )
+          break
+
+        default:
+          await m?.reply([postStream], `unknown command`)
+          break
+      }
+    }
   })
 
   socket.ws.on('message', (s: any) => {
-    console.log(s.toString())
+    // console.log(s.toString())
   })
-
-  //   socket.listen([postStream], s => {
-  //     socket.on('MessageCreated', async (e: any) => {
-  //       if (e.owner === ccid) return
-  //
-  //       const message: any = await client.getMessage(e.id, e.owner)
-  //
-  //       const command = message.body
-  //         .slice(commandPrefix.length)
-  //         .trimStart()
-  //         .split(' ')[0]
-  //
-  //       const args = message.body
-  //         .slice(commandPrefix.length + command.length + 1)
-  //         .trimStart()
-  //         .split(' ')
-  //
-  //       if (message.body && message.body.startsWith(commandPrefix)) {
-  //         console.log(`command: ${command}`)
-  //         console.log(`args: ${typeof args} ${JSON.stringify(args)}`)
-  //         console.log(`e.id, e.owner: ${e.id}, ${e.owner}`)
-  //
-  //         switch (command) {
-  //           case `ping`:
-  //             await client.reply(e.id, e.owner, [postStream], `pong`)
-  //             break
-  //
-  //           case 'echo':
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               args.join(' ').trimStart()
-  //             )
-  //             break
-  //
-  //           case 'cowsay':
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               '```' + cowsay.say({ text: args.join(' ') }) + '```'
-  //             )
-  //             break
-  //
-  //           case 'cowthink':
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               '```' + cowsay.think({ text: args.join(' ') }) + '```'
-  //             )
-  //             break
-  //
-  //           case 'roll':
-  //             const [count, sides] = args.join('').split('d').map(Number)
-  //             const arr: Number[] = Array.from(Array(Number(count)))
-  //             const res = arr.map(() => rollDice(sides.toString()))
-  //
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               `${count}個の${sides}面サイコロを振りました
-  //           合計: ${res.reduce((acc, cur) => acc + cur)}\n${res.join(' ')}`
-  //             )
-  //             break
-  //
-  //           case 'jpdict':
-  //             const proc = Bun.spawn(['myougiden', ...args])
-  //             const text = await new Response(proc.stdout).text()
-  //             await client.reply(e.id, e.owner, [postStream], text)
-  //             break
-  //
-  //           case 'current':
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               `只今${new Date().toLocaleTimeString('ja-JP')}でございます。`
-  //             )
-  //             break
-  //
-  //           case 'help':
-  //             await client.reply(
-  //               e.id,
-  //               e.owner,
-  //               [postStream],
-  //               '```' +
-  //                 `
-  // コマンド一覧:
-  //   ping: pongを返す
-  //     /con ping
-  //   cowsay: Linuxのcowsayコマンドと同じ
-  //     /con cowsay <text>
-  //   cowthink: Linuxのcowthinkコマンドと同じ
-  //     /con cowthink <text>
-  //   roll: サイコロを振る
-  //     /con roll <個数>d<面数>
-  //   jpdict: 辞書で単語を検索
-  //     /con jpdict <word>`
-  //             )
-  //             break
-  //
-  //           default:
-  //             await client.reply(e.id, e.owner, [postStream], `unknown command`)
-  //             break
-  //         }
-  // }
-  // })
-  // })
 })
